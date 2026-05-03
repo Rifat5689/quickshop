f1ad0454b2a02e9e6ad628e19765376333d944c3
Rifat Hossen <rifathossen56890@gmail.com>
2026-05-03 23:15:38 +0600

    update server to include database connection and add plans to .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import 'dotenv/config'
+import connectDb from "./src/database/db.js";
 
 const app = express() ; 
 const port = process.env.PORT || 3000  ; 
-
+connectDb() ;
 app.get('/',(req,res) =>{
       const a ='rifat' ; 
       res.send(a) ; 
